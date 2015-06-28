229c725f73d13e02dc766af26c29af31a079f13a
Jean de Klerk <jadekler@gmail.com>
2015-06-28 09:01:57 -0600

    wip annotations

--- java-annotations/src/main/java/com/foo/PrinterRunner.java
+++ java-annotations/src/main/java/com/foo/PrinterRunner.java
@@ -1,29 +1,68 @@
 package com.foo;
 
+import java.io.*;
 import java.lang.reflect.*;
+import java.net.*;
+import java.util.*;
+import java.util.jar.JarFile;
 
 public class PrinterRunner {
-    public static void main(String[] args) {
-        Class<TrivialPrinter> printerClass = TrivialPrinter.class;
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        Enumeration<URL> resources = classLoader.getResources("");
 
-        if (printerClass.isAnnotationPresent(Printer.class)) {
-            Printer printerAnnotation = printerClass.getAnnotation(Printer.class);
-            System.out.println("Running: " + printerAnnotation.name());
-            System.out.println("Purpose: " + printerAnnotation.purpose());
+        while (resources.hasMoreElements()) {
+            URL url = resources.nextElement();
+            System.out.println(urlit);
+            System.out.println("----");
+//
+//            File rootFile = new File(url.getPath());
+//            List<File> files = listFiles(rootFile);
+//            files.forEach(file -> {
 
-            for (Method method : printerClass.getDeclaredMethods()) {
-                if (method.isAnnotationPresent(Print.class)) {
-                    Print printAnnotation = method.getAnnotation(Print.class);
+                //                try {
+//                    Class<?> myClass = classLoader.loadClass(file.toString());
+//                    System.out.println(myClass.getCanonicalName());
+//                } catch (ClassNotFoundException e) {
+//                    e.printStackTrace();
+//                }
+//            });
+        }
 
-                    if (printAnnotation.enabled()) {
-                        try {
-                            method.invoke(printerClass.newInstance());
-                        } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
+        //        Class<TrivialPrinter> printerClass = TrivialPrinter.class;
+//
+//        if (printerClass.isAnnotationPresent(Printer.class)) {
+//            Printer printerAnnotation = printerClass.getAnnotation(Printer.class);
+//            System.out.println("Running: " + printerAnnotation.name());
+//            System.out.println("Purpose: " + printerAnnotation.purpose());
+//
+//            for (Method method : printerClass.getDeclaredMethods()) {
+//                if (method.isAnnotationPresent(Print.class)) {
+//                    Print printAnnotation = method.getAnnotation(Print.class);
+//
+//                    if (printAnnotation.enabled()) {
+//                        try {
+//                            method.invoke(printerClass.newInstance());
+//                        } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
+//                            e.printStackTrace();
+//                        }
+//                    }
+//                }
+//            }
+//        }
+    }
+
+    private static List<File> listFiles(File root) throws IOException {
+        List<File> files = new ArrayList<>();
+
+        for (File file : root.listFiles()) {
+            if (file.isDirectory()) {
+                files.addAll(listFiles(file));
+            } else {
+                files.add(file);
             }
         }
+
+        return files;
     }
 }
