5354a141001bedea9d21c6df233d83403f0ee192
Jean de Klerk <jadekler@gmail.com>
2015-09-23 21:34:00 -0600

    Small comment

--- java-gemfire-testing/utils/src/main/java/testing/GemfireTestingUtil.java
+++ java-gemfire-testing/utils/src/main/java/testing/GemfireTestingUtil.java
@@ -5,6 +5,8 @@ import com.gemstone.gemfire.cache.client.*;
 
 public class GemfireTestingUtil {
     public static Region getRegionConnection(String regionName, Class keyClass, Class valueClass) {
+        // Honestly, we could probably just use this to get a connection in our main app (after adding pdx serialization
+        // to the cache)
         ClientCacheFactory ccf = new ClientCacheFactory();
         ccf.addPoolLocator("localhost", 10334);
         ClientCache cc = ccf.create();
