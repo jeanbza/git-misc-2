fffdbcc62cf91cdbbd92db781f0195afc04f4799
Jean de Klerk <jadekler@gmail.com>
2015-10-26 20:15:11 -0600

    Private function for creating list items

--- react-browserify-bootstrap/src/NavPushRight.jsx
+++ react-browserify-bootstrap/src/NavPushRight.jsx
@@ -3,30 +3,63 @@ import 'jasny-bootstrap'
 
 export default class NavPushRight extends Component {
     render() {
+        const links = [
+            {
+                active: true,
+                url: '#',
+                text: 'Hello world!'
+            },
+            {
+                active: false,
+                url: '#',
+                text: 'Foo!'
+            },
+            {
+                active: false,
+                url: '#',
+                text: 'Bar!'
+            }
+        ]
+
         return (
             <nav className="navbar navbar-default">
                 <div className="container-fluid">
                     <div className="navbar-header">
-                        <button type="button" className="navbar-toggle" data-toggle="offcanvas"
-                                data-target=".navmenu"
-                                data-canvas="body">
+                        <button type="button" className="navbar-toggle"
+                                data-toggle="offcanvas" data-target=".navmenu" data-canvas="body">
+                            <span className="sr-only">Toggle navigation</span>
                             <span className="icon-bar"></span>
                             <span className="icon-bar"></span>
                             <span className="icon-bar"></span>
                         </button>
                         <a className="navbar-brand" href="#">Brand</a>
+                        <ul className="nav navbar-nav hidden-xs">
+                            {this._listItems(links)}
+                        </ul>
                     </div>
 
-                    <div className="navmenu navmenu-default navmenu-fixed-left offcanvas">
+                    <div className="offcanvas navmenu navmenu-default navmenu-fixed-left">
                         <a className="navmenu-brand" href="#">Project name</a>
                         <ul className="nav navmenu-nav">
-                            <li><a href="#">Link</a></li>
-                            <li><a href="#">Link</a></li>
-                            <li><a href="#">Foo!</a></li>
+                            {this._listItems(links)}
                         </ul>
                     </div>
                 </div>
             </nav>
         )
+    }
+
+    _listItems(links) {
+        return links.map(function(link, index) {
+            if (link.active) {
+                return (
+                    <li key={index} className="active"><a href={link.url}>{link.text}<span className="sr-only">(current)</span></a></li>
+                )
+            } else {
+                return (
+                    <li key={index}><a href={link.url}>{link.text}</a></li>
+                )
+            }
+        })
     }
 }
